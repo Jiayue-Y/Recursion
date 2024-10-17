92a26e926a1356d705f1102b333017d85e485d29
Jiayue-Y <yangphoebe2019@gmail.com>
2024-10-17 12:12:17 -0700

    TextWise reverse function

---
+++ src/TextWise.java
@@ -0,0 +1,13 @@
+public class TextWise {
+    static String reverse(String input){
+        if (input == null || input.length() <= 1) return input;
+        return reverse(input.substring(1)) + input.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        String input = "hello world!", output = reverse(input);
+        System.out.println("Original: " + input);
+        System.out.println("Reversed: " + output);
+    }
+
+}
